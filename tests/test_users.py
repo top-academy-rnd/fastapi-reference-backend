ba2ca80e89cc25a58a32891b5b6a94d1489ab2f7
Your Name <you@example.com>
2026-07-13 21:34:40 +0300

    Implement one session test

--- tests/test_users.py
+++ tests/test_users.py
@@ -21,14 +21,35 @@ def test_registration():
 def get_registered_user():
     """ Регистрирует пользователя, возвращает логин и пароль"""
 
-    pass
+    test_login = f"test-{uuid4()}"
+    test_password = str(uuid4())
+    r = requests.post(
+        "http://127.0.0.1:8000/users",
+        json={
+            "login": test_login,
+            "password": test_password,
+        },
+    )
+    assert r.status_code == 200  # но в принципе этот assert можно не делать
+
+    data = r.json()
+    return test_login, test_password, data["id"]
 
 
 def test_create_session_incorrect_password():  # проверяем вход с некорректным паролем
     # регистрируете пользователя через функцию
     # на этом пользователе делаете проверку
 
-    pass
+    test_login, test_password, test_user_id = get_registered_user()
+
+    r = requests.post(
+        "http://127.0.0.1:8000/sessions",
+        headers={
+            "login": test_login,
+            "password": test_password + "incorrect",
+        },
+    )
+    assert r.status_code == 401
 
 
 def test_create_session_correct_password():  # проверяем вход с корректным паролем
